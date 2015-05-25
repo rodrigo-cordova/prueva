50d019f4320f85b6755ce5848e00b11c9b3ee468
rodrigo-cordova <rodrigo-cordova@users.noreply.github.com>
2015-05-25 14:56:14 -0300

    hize algunos cambios

--- calculadora.c
+++ calculadora.c
@@ -5,6 +5,7 @@ int main(){
 	printf("Ingrese a un familiar \n");
 	scanf("%s",c);
 	printf("lacadena dice %s \n",c);
-	printf("BUENO eso fue toso\n");
+	printf("BUENO eso fue toso\n");//me parece que eso no iba 
+	printf("examenes finales \n");
 	return 0;
 }
